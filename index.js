c8f20f5512c8525763aba821f204ad224d046b5b
[sk developer] <[skdeveloper101@gmail.com]>
2024-06-27 17:15:10 +0530

    Set up project

---
+++ index.js
@@ -0,0 +1,9 @@
+// Features - BMI calculator and feedback with automatic unit conversion
+// units (height) - feet, metre, inch
+// units (weight) - pound, kilogram
+
+// Generate code for UI using AI
+// Create a unit convertor program
+// create a BMI calculator function
+// Create a feedback function
+// Integrate all of the things
